01968b6bb1a515a3e58fd7ac339498c47bf942d5
Jayde Mirandilla <110360105+kairos0101@users.noreply.github.com>
2024-05-10 04:58:57 +0800

    Add files via upload

---
+++ PyTorch/PyTorch-Lightning/9. Profiler/config.py
@@ -0,0 +1,15 @@
+# Training Hyperparameters
+INPUT_SIZE = 784
+NUM_CLASSES = 10
+LEARNING_RATE = 0.001
+BATCH_SIZE = 64
+NUM_EPOCHS = 1
+
+# Dataset
+DATA_DIR = "dataset/"
+NUM_WORKERS = 0
+
+# Compute related
+ACCELERATOR = "mps"
+DEVICES = [0]
+PRECISION = 16
